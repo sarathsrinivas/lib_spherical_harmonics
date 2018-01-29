75c38ad9451ddf36e752bec37bd096d704cbef26
sarath srinivas s <srinix@protonmail.com>
2018-01-29 23:41:15 +0530

    Moved around tests.

--- lib_spherical.h
+++ lib_spherical.h
@@ -1,6 +1,7 @@
 #include <complex.h>
 /*LEBEDEV GRID */
 int get_lebedev_grid(double **theta, double **phi, double **wleb, int nleb, unsigned long *ngrid, char *path);
+double test_get_lebedev_grid(int leb, int tfun, char *leb_path);
 /* SPHERICAL HARMONICS */
 double ylm_real(int l, int m, double th, double phi);
 double *get_ylm_real_matrix(int lmax, double *th, double *phi, unsigned long nleb);
@@ -14,11 +15,10 @@ double complex *get_ylm_matrix(int lmax, double *th, double *phi, unsigned long 
 double complex *get_sph_coeff(double *v, double *wleb, double complex *y, unsigned long nleb, unsigned long nl);
 double complex *get_ylm_orthonormality(double *wleb, double complex *y, unsigned long nl, unsigned long nleb);
 double complex *get_rev_project(double complex *vll, double complex *y, unsigned long nl, unsigned long nleb);
-/* ROATATION */
-int rodrigues_rot_3d(double *v, double *z, double *vr);
-/* TESTS */
-double test_get_lebedev_grid(int leb, int tfun, char *leb_path);
 double test_ylm_orthonormality(int leb, int lmax, char *leb_path);
 double test_ylm_real_orthonormality(int leb, int lmax, char *leb_path);
 double test_ylm_real_projection_1d(unsigned long np, int leb, int lmax, int tfun, char *path);
 double test_ylm_real_projection_2d(unsigned long np, int leb, int lmax, int tfun, char *path);
+/* ROATATION */
+int rodrigues_rot(const double *v, double th, const double *n, double *vr);
+double test_rodrigues_rot(unsigned long nt);
